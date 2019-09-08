4fcf51d640c77bcf822b73bc6d12bbc976920559
shahramam <kooroosh2020aa@gmail.com>
2019-09-08 15:43:42 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -71,7 +71,7 @@ function create_config( )
     "plugins",
     "tools"
  },
-    sudo_users = {283392246},
+    sudo_users = {886812924,541632594},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
